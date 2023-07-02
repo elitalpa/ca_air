7e4b1b609ec9a6b8503b741819c89fa0bf5809fd
elitalpa <elitalpa@gmail.com>
2023-07-02 22:25:34 +0200

    air00.py - starting the split exercise

--- air00.py
+++ air00.py
@@ -9,12 +9,31 @@
 #
 # Afficher error et quitter le programme en cas de problèmes d’arguments.
 
+import sys
+
 ### Functions ###
+def my_function(string_to_cut, string_separator):
+    array = string_to_cut
+    # algo
+    return array
+
+def handle_argument_errors():
+    if len(sys.argv) != 2:
+        print("error: Only one argument is allowed.")
+        exit()
+    if sys.argv[1].strip('-').isdigit():
+        print("error: Please provide a string argument.")
+        exit()
 
 ### Error Handling ###
+handle_argument_errors()
 
 ### Parsing ###
+string_input = sys.argv[1]
+string_separator = [" "]
 
 ### Problem Solving ###
+function_result = my_function(string_input, string_separator)
 
 ### Result ###
+print(function_result)
