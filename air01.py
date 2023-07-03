01f00b0110bc865bae597907af30ad00aea0c576
elitalpa <elitalpa@gmail.com>
2023-07-03 23:51:34 +0200

    air01.py - preparing split in function exercise

--- air01.py
+++ air01.py
@@ -8,13 +8,31 @@
 ##     return tableau
 #
 # Afficher error et quitter le programme en cas de problèmes d’arguments.
+import sys
 
 ### Functions ###
+def my_function(string_to_cut, string_separator):
+    array = string_to_cut
+    # algo
+    return array
+
+def handle_argument_errors():
+    if len(sys.argv) != 3:
+        print("error: Two arguments are needed.")
+        exit()
+    if sys.argv[1].strip('-').isdigit() or sys.argv[2].strip('-').isdigit():
+        print("error: Your arguments must be strings.")
+        exit()
 
 ### Error Handling ###
+handle_argument_errors()
 
 ### Parsing ###
+string_input = sys.argv[1]
+string_separator = [chr(32), chr(10), chr(9)]
 
 ### Problem Solving ###
+function_result = my_function(string_input, string_separator)
 
 ### Result ###
+print(function_result)
