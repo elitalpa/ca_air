1a64b0dba1c3b98b91f622cea18565b6ee51f37c
elitalpa <elitalpa@gmail.com>
2023-07-08 00:23:09 +0200

    air07.py - starting exercise

--- air07.py
+++ air07.py
@@ -10,12 +10,37 @@
 #
 # Afficher error et quitter le programme en cas de problèmes d’arguments.
 
+import sys
+
 ### Functions ###
+def sorted_insert(array, new_element):
+    new_array = array[:]
+    # algo
+    return new_array
+
+def handle_argument_errors():
+    if len(sys.argv) < 4:
+        print("error: At least three arguments are needed.")
+        exit()
+
+    for arg in sys.argv[1:-1]:
+        if not arg.strip('-').isdigit():
+            print("error: Your arguments must be integers.")
+            exit()
+
+    if not sys.argv[-1::][0][0].strip('-').isdigit():
+        print("error: Your last argument must be an integer.")
+        exit()
 
 ### Error Handling ###
+handle_argument_errors()
 
 ### Parsing ###
+array = sys.argv[1:-1]
+new_element = sys.argv[-1::][0][0]
 
 ### Problem Solving ###
+sorted_insert_result = sorted_insert(array, new_element)
 
 ### Result ###
+print(sorted_insert_result)
