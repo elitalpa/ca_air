85c496d72350682ba0d87258f50d68b514f11de9
elitalpa <elitalpa@gmail.com>
2023-07-11 16:02:27 +0200

    air11.py - show a pyramid exercise is done

--- air11.py
+++ air11.py
@@ -4,12 +4,43 @@
 #
 # Afficher error et quitter le programme en cas de problèmes d’arguments.
 
+import sys
+
 ### Functions ###
+def generate_pyramid(char, number_of_floors):
+    number_of_floors_int = int(number_of_floors)
+    pyramid = []
+
+    for floor in range(1, number_of_floors_int + 1):
+        spaces = ' ' * (number_of_floors_int - floor)
+        line = spaces + char * (2 * floor - 1) + spaces
+
+        pyramid.append(line + '\n')
+
+    return pyramid
+
+def handle_argument_errors():
+    if len(sys.argv) != 3:
+        print("error: Two arguments are required: the charachter and the number of floors for your pyramid.")
+        exit()
+
+    if len(sys.argv[1]) != 1:
+        print("error: Your first argument can only be one charachter.")
+        exit()
+
+    if not sys.argv[2].isdigit() or int(sys.argv[2]) <= 0:
+        print("error: Your second argument needs to be a positive integer above 0.")
+        exit()
 
 ### Error Handling ###
+handle_argument_errors()
 
 ### Parsing ###
+input_char = sys.argv[1]
+input_floors = sys.argv[2]
 
 ### Problem Solving ###
+generated_pyramid_result = generate_pyramid(input_char, input_floors)
 
 ### Result ###
+print(''.join(generated_pyramid_result), end="")
