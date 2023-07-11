9855c9bf6845c763e173664e3efa87880d854688
elitalpa <elitalpa@gmail.com>
2023-07-11 14:23:28 +0200

    air08.py - merging two sorted arrays exercise is done

--- air08.py
+++ air08.py
@@ -13,9 +13,23 @@ import sys
 
 ### Functions ###
 def sorted_fusion(array1, array2):
-    new_array = array1, array2
+    array1 = [int(n) for n in array1]
+    array2 = [int(n) for n in array2]
 
-    # algo
+    new_array = []
+    i = 0
+    j = 0
+
+    while i < len(array1) and j < len(array2):
+        if array1[i] < array2[j]:
+            new_array.append(array1[i])
+            i += 1
+        else:
+            new_array.append(array2[j])
+            j += 1
+
+    new_array += array1[i:]
+    new_array += array2[j:]
 
     return new_array
 
@@ -31,11 +45,20 @@ def handle_argument_errors():
             contains_fusion_string = True
 
         if not arg.strip('-').isdigit() and not arg == "fusion":
-            print('error: All of your arguments must be integers except the "fusion" string which separates the lists of integers.')
+            print('error: All of your arguments must be integers except the "fusion" string which separates the two sorted lists of integers.')
             exit()
 
     if not contains_fusion_string:
         print('error: The "fusion" string is missing.')
+        exit()
+    
+    fusion_index = sys.argv[1:].index("fusion") + 1
+
+    input_array1 = sys.argv[1:fusion_index]
+    input_array2 = sys.argv[fusion_index + 1:]
+
+    if len(input_array1) < 2 or len(input_array2) < 2:
+        print('error: There must be at least two numbers before and after the "fusion" string.')
         exit()
 
 ### Error Handling ###
