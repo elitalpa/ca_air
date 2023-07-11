a1f5bea6b2a5bee355d7d2c626299e264d61be8b
elitalpa <elitalpa@gmail.com>
2023-07-11 14:33:17 +0200

    air09.py - starting exercise

--- air09.py
+++ air09.py
@@ -9,12 +9,29 @@
 #
 # Afficher error et quitter le programme en cas de problèmes d’arguments.
 
+import sys
+
 ### Functions ###
+def my_rotation(array):
+    new_array = []
+
+    # algo
+
+    return new_array
+
+def handle_argument_errors():
+    if len(sys.argv) < 3:
+        print("error: At least 2 arguments are required.")
+        exit()
 
 ### Error Handling ###
+handle_argument_errors()
 
 ### Parsing ###
+input_array = sys.argv[1:]
 
 ### Problem Solving ###
+my_rotation_result = my_rotation(input_array)
 
 ### Result ###
+print(" ".join(my_rotation_result))
