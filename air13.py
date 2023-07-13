cbda8813aadd746727c37ec42075f18911d63559
elitalpa <elitalpa@gmail.com>
2023-07-13 23:45:51 +0200

    air13.py - preparing exercise

--- air13.py
+++ air13.py
@@ -7,12 +7,26 @@
 # Bonus : trouvez le moyen d’utiliser du vert et du rouge
 # pour rendre réussites et échecs plus visibles.
 
+import sys
+
 ### Functions ###
+def test_exercises():
+    test_result = ""
+    # algo
+    return test_result
+
+def handle_argument_errors():
+    if len(sys.argv) > 1:
+        print("error: No arguments required.")
+        exit()
 
 ### Error Handling ###
+handle_argument_errors()
 
 ### Parsing ###
 
 ### Problem Solving ###
+test_exercises_result = test_exercises()
 
 ### Result ###
+print(test_exercises_result)
